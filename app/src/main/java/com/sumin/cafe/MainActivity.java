1db73039e70900c3b82d7cfee06917d5971da044
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-03-12 23:33:02 +0300

    Second_Screen_Design

--- app/src/main/java/com/sumin/cafe/MainActivity.java
+++ app/src/main/java/com/sumin/cafe/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +28,25 @@ public class MainActivity extends AppCompatActivity {
                 String userName = editTextName.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
 
-                Intent intent = new Intent(MainActivity.this, MakeOrderActivity.class);
-                intent.putExtra("userName", userName);
-                startActivity(intent);
+                if (userName.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(
+                            MainActivity.this,
+                            getString(R.string.error_fields_empty),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                } else {
+                    launchNextScreen(userName);
+                }
+
             }
         });
     }
 
+    private void launchNextScreen(String userName) {
+        Intent intent = new Intent(MainActivity.this, MakeOrderActivity.class);
+        intent.putExtra("userName", userName);
+        startActivity(intent);
+    }
     private void initViews() {
         editTextName = findViewById(R.id.editTextName);
         editTextPassword = findViewById(R.id.editTextPassword);
