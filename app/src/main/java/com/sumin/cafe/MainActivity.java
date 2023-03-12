e3bccc45ef7ebd358447e2e9d548d3e48d9302bc
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-03-12 23:21:54 +0300

    Second_Screen_Design

--- app/src/main/java/com/sumin/cafe/MainActivity.java
+++ app/src/main/java/com/sumin/cafe/MainActivity.java
@@ -4,14 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonSignIn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, MakeOrderActivity.class);
-        startActivity(intent);
+        initViews();
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = editTextName.getText().toString();
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
     }
 }
