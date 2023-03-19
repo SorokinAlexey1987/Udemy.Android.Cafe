3e1fc93b36910b8293ac2425d369799b89105c57
Sorokin Aleksei <alexey-sor@yandex.ru>
2023-03-19 15:06:17 +0300

    Second_Screen_Design

--- app/src/main/java/com/sumin/cafe/MakeOrderActivity.java
+++ app/src/main/java/com/sumin/cafe/MakeOrderActivity.java
@@ -5,13 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MakeOrderActivity extends AppCompatActivity {
+
+    private TextView textViewGreetings;
+    private TextView textViewAdditives;
+
+    private RadioGroup radioGroupDrinks;
+    private RadioButton radioButtonTea;
+    private RadioButton radioButtonCoffee;
+
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxLemon;
+
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+
+    private Button buttonMakeOrder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+        initViews();
+    }
+
+    private void initViews() {
+        textViewGreetings = findViewById(R.id.textViewGreetings);
+        textViewAdditives = findViewById(R.id.textViewAdditives);
+
+        radioGroupDrinks = findViewById(R.id.radioGroupDrinks);
+        radioButtonTea = findViewById(R.id.radioButtonTea);
+        radioButtonCoffee = findViewById(R.id.radioButtonCoffee);
+
+        checkBoxSugar = findViewById(R.id.checkBoxSugar);
+        checkBoxMilk = findViewById(R.id.checkBoxMilk);
+        checkBoxLemon = findViewById(R.id.checkBoxLemon);
+
+        spinnerTea = findViewById(R.id.spinnerTea);
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
+
+        buttonMakeOrder = findViewById(R.id.buttonMakeOrder);
     }
 
     public static Intent newIntent(Context context, String userName) {
